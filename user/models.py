456affd7836c3c66fcc68f8802d204890bd2884c
Alan-aj <alanj14333@gmail.com>
2022-03-22 12:38:35 +0530

    CRUD operations added
    connect to postgresSql and perform create, read, delete and update

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class signup(models.Model):
+    signid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=50)
--- user/views.py
+++ user/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
+from .models import signup
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
-
-def main(request):
-    return render(request,'main.html')
-
 def jquery(request):
     return render(request,'jqury.html')
 
-def form(request):
-    return render(request,'form.html')
-
 def fb(request):
     return render(request,'fb.html')
+
+
+# view to update and delete data from table signup
+def main(request):
+    data=signup.objects.all()
+    if request.method=='POST':
+        id=request.POST['id']
+        obj=signup.objects.filter(signid=id)
+        obj.delete()
+    return render(request,'main.html',{'user':data})
+
+# view to insert data into table signup
+def form(request):
+    if request.method=='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        pwd = request.POST['pwd']
+        obj = signup(name=name, email=email, password=pwd)
+        obj.save()
+
+    return render(request,'form.html')
